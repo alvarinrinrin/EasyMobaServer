8b53c9891d959db2e0961052fd6400c70eed617e
alvarinrinrin <alvarodrow@gmail.com>
2014-06-12 13:04:31 +0100

    Create BattleField.java

---
+++ BattleField.java
@@ -0,0 +1,18 @@
+package easymobaserver;
+
+class BattleField 
+{
+    int width;
+    int height;
+    //color image o whatever
+    
+    BattleField(int w, int h)
+    {
+        width = w;
+        height = h;
+    }
+    
+    public void paint()
+    {
+    }
+}
